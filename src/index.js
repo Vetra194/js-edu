ead37ad501879839bd9af4311c10816db9a6f799
Vetra194 <m_sol88@mail.ru>
2019-10-11 23:09:01 +0400

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -8,7 +8,14 @@ module.exports = function getTimeForEducation(
     focus = 'family', 
     knowsProgramming = true,
     config = {family: 4}
-    ) {
-      return 0;
-  };
-  
+    )
+    {
+      if (knowsProgramming) {
+        return Math.ceil(800/config[focus]);
+      } else {
+        return Math.ceil(1300/config[focus]);
+      }
+    } 
+    
+
+    
